0ca5db7118c9504862864e784e3ed114664107aa
fred <flynnezhang@gmail.com>
2020-07-26 01:22:29 -0400

    update consumer module

---
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/config/ApplicationContextConfig.java
@@ -0,0 +1,26 @@
+package com.atguigu.springcloud.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author Fred Zhang
+ * @create 2020-07-26 12:23 AM
+ */
+@Configuration
+public class ApplicationContextConfig {
+    @Bean
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+    /**
+     * @Configuration与@Bean结合使用。@Configuration可理解为用spring的时候xml里面的<beans>标签，
+     * @Bean可理解为用spring的时候xml里面的<bean>标签。Spring Boot不是spring的加强版，
+     * 所以@Configuration和@Bean同样可以用在普通的spring项目中，而不是Spring Boot特有的，
+     * 只是在spring用的时候，注意加上扫包配置。
+     *
+     * applicationContext.xml  <bean id="" class=""></bean>
+
+     */
+}
--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -1,7 +1,14 @@
 package com.atguigu.springcloud.controller;
 
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
 
 /**
  * @author Fred Zhang
@@ -10,4 +17,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Slf4j
 public class OrderController {
+
+    public static final String PAYMENT_URL="http://localhost:8001";
+    @Resource
+    private RestTemplate restTemplate;
+
+    @GetMapping("/consumer/payment/create") //Because it comes from front-end side, always GetMapping
+    public CommonResult<Payment> create(Payment payment){
+        //浏览器只能发get请求，但是底层的实质是调用服务端的Post
+        return restTemplate.postForObject(PAYMENT_URL+"/payment/create",payment,CommonResult.class);
+    }
+    @GetMapping("/consumer/payment/get/{id}")
+    public CommonResult<Payment> getPayment(@PathVariable("id") Long id){
+
+        return restTemplate.getForObject(PAYMENT_URL+"/payment/get/"+id,CommonResult.class);
+    }
 }
