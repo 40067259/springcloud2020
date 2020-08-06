997328ddde49c5aec71426bb17af653778f16ade
fred <flynnezhang@gmail.com>
2020-08-06 11:26:07 -0400

    added openfeign

---
+++ cloud-consumer-feign-order80/src/main/java/com/atguigu/springcloud/OrderFeignMain80.java
@@ -0,0 +1,17 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author Fred Zhang
+ * @create 2020-08-05 9:11 PM
+ */
+@SpringBootApplication
+@EnableFeignClients //使用feign激活并开启
+public class OrderFeignMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderFeignMain80.class,args);
+    }
+}
